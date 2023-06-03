7b83b6f17d8378b93ffb94766b942588b7b0173a
Tuğberk Ata <atatugberk@yahoo.com>
2023-06-03 18:45:09 +0300

    Initial Commit

---
+++ TimeConversionChallenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TimeConversionChallenge
+//
+//  Created by Tuğberk Ata on 3.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeConversionChallenge/TimeConversionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConversionChallengeApp.swift
+//  TimeConversionChallenge
+//
+//  Created by Tuğberk Ata on 3.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeConversionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
